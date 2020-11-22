6e06a0d5701bad53f63af54366b4833df2b3df86
Hasija <sankalpinfotech96@gmail.com>
2020-11-22 21:57:58 +0530

    form done using simple react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    }
+  }
+  render() {
+    const changeHandler = (event) => {
+      const { name, value } = event.target;
+      this.setState({ ...this.state, [name]: value })
+    }
+    return (
+      <>
+        <div className="row mt-5" >
+          <div className="col-md-3">
+          </div>
+          <div className="col-md-6">
+            <form>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={changeHandler} value={this.state.email} />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={this.state.password} onChange={changeHandler} />
+              </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
+          </div>
+          <div className="col-md-3"> </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4"></div>
+          <div className="col-md-4">
+            <h4>Email:{this.state.email}</h4>
+            <h4>Password:{this.state.password}</h4>
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </>
+    );
+  }
+
 }
 
 export default App;
